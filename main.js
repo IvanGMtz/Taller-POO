7c0e0a8c069c158df678ad9314d18a94ee2b8feb
IvanGMtz <navi.garces24@gmail.com>
2023-05-27 14:37:36 -0500

    feat: :zap: Excercise 5

--- main.js
+++ main.js
@@ -1,73 +1,46 @@
-class Vehiculo {
-    marca;
-    modelo;
-    velocidad;
-    constructor({ marca, modelo, velocidad}) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.velocidad = velocidad;
+class Empleado {
+    nombre;
+    edad;
+    sueldo;
+    static id;
+    constructor({ nombre, edad, sueldo, id}) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldo = sueldo;
+        this.id=id
     }
-    acelerar() {
-        return `Velocidad = ${this.velocidad*10} [km/h]🚗💨`
+    calcularSalarioAnual() {
+        return `Salario anual = $${(this.sueldo*12).toLocaleString()}`
     }
+
 }
 
-class Coche extends Vehiculo {
-    combustible;
-    constructor({ marca, modelo, velocidad }) {
-        super({marca, modelo, velocidad})
-        this.combustible = combustible;
+class Gerente extends Empleado {
+    constructor({ nombre, edad, sueldo }) {
+        super({nombre, edad, sueldo})
     }
-    acelerar() {
-        return `Velocidad = ${this.velocidad*20} [km/h]🚗💨`
-    }
-
-    static convertirKmHEnMph(p1){
-        return `Velocidad = ${(p1/1.60934).toFixed(2)} [Mph]`;
+    calcularSalarioAnual() {
+        return `Salario anual = $${(this.sueldo*12*1.1).toLocaleString()}`
     }
 }
 
 const Tipo = document.getElementById("tipo");
 
-document.addEventListener("change", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    
-    const Combustible = document.getElementById("combustible");
-    const BtnConvertir = document.getElementById("convertir");
-
-    if (e.target.id == "tipo") {
-        if (Tipo.value == "vehiculo") {
-            Combustible.style.display = "none";
-            BtnConvertir.style.display = "none";
-            
-        } else if (Tipo.value == "coche") {
-            Combustible.style.display = "block";
-            BtnConvertir.style.display = "block";
-            
-        } else {
-            Combustible.style.display = "none";
-            BtnConvertir.style.display = "none";         
-        }
-    }
-});
-
 document.addEventListener("click", e => {
-    let newMarca = document.getElementById("marca").value;
-    let newModelo = document.getElementById("modelo").value;
-    let newVelocidad = document.getElementById("velocidad").value;
-    let newCombustible = document.getElementById("combustible").value;
-    if (e.target.id == "acelerar") {
-        if(Tipo.value == "vehiculo"){
-            const newVehiculo = new Vehiculo({ marca: newMarca, modelo: newModelo, velocidad: newVelocidad});
-            document.getElementById("text").innerHTML = `${newVehiculo.acelerar()}`;
-        } else if (Tipo.value == "coche"){
-            const newCoche = new Coche({ marca: newMarca, modelo: newModelo, velocidad: newVelocidad, combustible: newCombustible});
-            document.getElementById("text").innerHTML = `${newCoche.acelerar()}`;
+    let newnombre = document.getElementById("nombre").value;
+    let newedad = document.getElementById("edad").value;
+    let newsueldo = document.getElementById("sueldo").value;
+
+    const newEmpleado = new Empleado({ nombre: newnombre, edad: newedad, sueldo: newsueldo});
+    const newGerente = new Gerente({ nombre: newnombre, edad: newedad, sueldo: newsueldo, combustible: newCombustible});
+
+    if (e.target.id == "calcular") {
+        if(Tipo.value == "empleado"){
+            document.getElementById("text").innerHTML = `${newEmpleado.calcularSalarioAnual()}`;
+        } else if (Tipo.value == "gerente"){
+            
+            document.getElementById("text").innerHTML = `${newGerente.calcularSalarioAnual()}`;
         }
         
-    } else if (e.target.id == "convertir") {
-        const newCoche = new Coche({ marca: newMarca, modelo: newModelo, velocidad: newVelocidad, combustible: newCombustible});
-        document.getElementById("text").innerHTML = `${Coche.convertirKmHEnMph(newCoche.velocidad)}`;
     }
 })
